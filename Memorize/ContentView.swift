10f7d656ddd050b72525ad19e8c0cf235f97fb7e
Saad Kashif <saad220905@gmail.com>
2023-12-09 17:23:05 -0500

    finished lecture # 2

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -8,40 +8,90 @@
 import SwiftUI
 
 struct ContentView: View {
+    let emojis: Array<String>/*[String]*/ = ["👻", "🎃", "🕷️", "😈", "💀", "🕸️", "🧙", "🙀", "👹", "😱", "☠️", "🍭"]
+    
+    @State var cardCount: Int = 4
+    
     var body: some View {
 //        VStack (alignment: .leading, spacing: 20) {
-        HStack() {
-            CardView(isFaceUp: true)
-            CardView()
-            CardView()
-            CardView()
+        VStack {
+            ScrollView {
+                cards
+            }
+            Spacer()
+            cardCountAdjusters
         }
-        .foregroundStyle(.green)
         .padding()
-        .background(.gray)
+//      .background(.gray)
     }
-}
-
-struct CardView: View {
-    var isFaceUp: Bool = false //isFaceUp has been defaulted to false
-    var body: some View {
-        ZStack () {
-            if isFaceUp{
-                RoundedRectangle(cornerRadius: 12)
-                    .foregroundColor(.white)
-                RoundedRectangle(cornerRadius: 12)
-                    .strokeBorder(style: StrokeStyle(lineWidth: 2, dash: [10,2]))
-                Text("👻").font(.largeTitle)
-            } else {
-                RoundedRectangle(cornerRadius: 12)
+    
+    var cards: some View {
+        LazyVGrid (columns: [GridItem(.adaptive(minimum: 120))]){
+            ForEach(0..<cardCount/*emojis.indices*/, id: \.self) { index in
+                CardView(content: emojis[index])
+                    .aspectRatio(2/3, contentMode: .fit)
             }
         }
+        .foregroundStyle(.orange)
+    }
+    
+    var cardCountAdjusters: some View {
+        HStack {
+            cardRemover
+            Spacer()
+            cardAdder
+        }
+        .imageScale(.large)
+        .font(.largeTitle)
+    }
+    
+    func cardCountAdjuster(by offset: Int, symbol: String) -> some View {
+        Button(action: {
+            cardCount += offset
+        }, label: {
+            Image(systemName: symbol)
+        })
+        .controlSize(.mini)
+        .disabled(cardCount + offset < 1 || cardCount + offset > emojis.count)
+    }
+    
+    var cardRemover: some View {
+        cardCountAdjuster(by: -1, symbol: "rectangle.stack.badge.minus.fill")
+    }
+    
+    var cardAdder: some View {
+        cardCountAdjuster(by: 1, symbol: "rectangle.stack.badge.plus.fill")
     }
 }
-
-
-
-
+//every var in a struct that's called has to be assigned a value,
+//either in its struct or in the call.
+struct CardView: View {
+    var content: String
+    //swift is a strongly typed language, so you don't necessarily have to
+    //indicate the type
+    @State var isFaceUp: Bool = false //isFaceUp has been defaulted to false
+    var body: some View {
+        ZStack (alignment: .top) {
+            let base = RoundedRectangle(cornerRadius: 12) //an example of a local variable
+            Group {
+                base
+                    .foregroundColor(.white)
+                base
+                    .strokeBorder(style: StrokeStyle(lineWidth: 2, dash: [10,2]))
+                Text(content)
+                    .font(.largeTitle)
+            }
+            .opacity(isFaceUp ? 1 : 0)
+            base
+                .fill()// the fill is basically the default, we don't need it
+                .opacity(isFaceUp ? 0 : 1)
+            
+        }
+        .onTapGesture {
+            isFaceUp.toggle()// = !isFaceUp
+        }
+    }
+}
 
 
 
